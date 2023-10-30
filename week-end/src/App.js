1fabe9e37d45592cefa2aa7c13710ee29a1ef7e0
ek-sarath <sarath.e@edstem.com>
2023-10-30 11:11:29 +0530

    Completed

--- week-end/src/App.js
+++ week-end/src/App.js
@@ -1,78 +1,123 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  const [post, setPost] = useState([]);
-  const [newPostText, setNewPostText] = useState('');
+ function App() {
+   const [posts, setPosts] = useState([]);
+   const [newPostText, setNewPostText] = useState('');
+   const [editingPostId, setEditingPostId] = useState(null); 
 
-  const handlePostChange = (e) => {
-    setNewPostText(e.target.value);
-  };
 
-  const handlePostSubmit = () => {
-    if (newPostText.trim() !== '') {
-      const newPost = {
-        id: post.length + 1,
-        text: newPostText,
-        likes: 0,
-        unlikes: 0,
-      };
-      setPost([...post, newPost]);
-      setNewPostText('');
-    }
-  };
+   const handlePostChange = (e) => {
+    setNewPostText(e.target.value);};
+
+
+   const handlePostSubmit = () => {
+     if (newPostText.trim() !== '') {
+       const newPost = {
+         id: Date.now(),
+         text: newPostText,
+         likes: 0,
+         unlikes: 0,
+         isEditing: false,};
+      setPosts([...posts, newPost]);
+      setNewPostText('');}};
+
 
   const handleLike = (postId) => {
-    const updatedPosts = post.map((p) => {
-      if (p.id === postId) {
-        return { ...p, likes: p.likes + 1 };
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes+1};
       }
-      return p;
-    });
-    setPost(updatedPosts);
-  };
+      return post;});
+    setPosts(updatedPosts);};
+
 
   const handleUnlike = (postId) => {
-    const updatedPosts = post.map((p) => {
-      if (p.id === postId) {
-        return { ...p, unlikes: p.unlikes + 1 };
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return {...post, unlikes: post.unlikes+1};
       }
-      return p;
+      return post;
     });
-    setPost(updatedPosts);
+    setPosts(updatedPosts);
+  }; 
+
+
+  const handleEdit = (postId) => {
+    setEditingPostId(postId);
+
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, isEditing: true };
+      }
+      return post;});
+    setPosts(updatedPosts);};
+    
+
+  const handleSaveEdit = (postId, newText) => {
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, text: newText, isEditing: false };
+      }
+      return post;});
+    setPosts(updatedPosts);
+    setEditingPostId(null);};
+
+
+  const handleCancelEdit = (postId) => {
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, isEditing: false };
+      }
+      return post;});
+    setPosts(updatedPosts);
+    setEditingPostId(null);};
+ 
+
+  const handleDelete = (postId) => {
+    const updatedPosts = posts.filter((post) => post.id !== postId);
+    setPosts(updatedPosts);
+    if (editingPostId === postId) {
+      setEditingPostId(null);}
   };
+
+
 
   return (
     <div className="App">
-      <h1>Mini Social Media Feed</h1>
-      <div id="PostForm" className="PostForm">
-        <textarea
-          rows="4"
-          className="PostBox"
-          placeholder="What's on your mind?"
-          value={newPostText}
-          onChange={handlePostChange}
-        />
-        <button onClick={handlePostSubmit} className="PostButton">
-          Post
-        </button>
-      </div>
 
-      <div>
-        {post.map((p) => (
-          <div className="Post" key={p.id}>
-            <p>{p.text}</p>
-            <div>
-              <span className="LikeButton" onClick={() => handleLike(p.id)}>
-                👍Likes({p.likes})
-              </span>
-              <span className="UnlikeButton" onClick={() => handleUnlike(p.id)}>
-                👎Dislike({p.unlikes})
-              </span>
-            </div>
-          </div>
-        ))}
-      </div>
+        <h1>Mini Social Media Feed</h1>
+
+        <div className="PostForm">
+          <textarea rows="4" className="PostBox" placeholder="What's on your mind?" value={newPostText} onChange={handlePostChange}/>
+          <button onClick={handlePostSubmit} className="PostButton"> Post </button>
+       </div>
+
+        <div>
+            {posts.map((post) => (
+              <div className="Post" key={post.id}>
+                {post.isEditing ? (
+
+                <div>
+                  <textarea value={newPostText} onChange={(e) => setNewPostText(e.target.value)}/>
+                  <button onClick={() => handleSaveEdit(post.id, newPostText)} className='SaveButton'>Save</button>
+                  <button onClick={() => handleCancelEdit(post.id)} className='CancelButton'>Cancel</button>
+                </div>
+
+                 ) : (<p>{post.text}</p>)}
+                   
+                <div>
+                  <span className="LikeButton" onClick={() => handleLike(post.id)}>👍Likes({post.likes})</span>
+                  <span className="UnlikeButton" onClick={() => handleUnlike(post.id)}>👎Dislike({post.unlikes})</span>
+                  {post.isEditing ? null : (
+                  <button onClick={() => handleEdit(post.id)} className='EditButton'>Edit</button>
+                  )}
+                  <button onClick={() => handleDelete(post.id)} className='DeleteButton'>Delete</button>
+                </div>
+
+              </div>
+          ))}
+        </div>
     </div>
   );
 }
